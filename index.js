37e560cf5e44d2e9d7b9aa2583867eeddc985d3d
Vaibhavihole31 <itspinki05@gmail.com>
2023-03-21 08:02:31 +0530

    ref: jwt token code

--- index.js
+++ index.js
@@ -1,58 +1,47 @@
 import express from "express";
 import jwt from 'jsonwebtoken';
 
-const secretKey = "pinks-rtc"
 const app = express();
+app.use(express.json())
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "a simple api"
-    })
-})
+const SECRET_KEY = "pinks-rtc"
 
-app.post("/login", (req,res) => {
-    const user = {
-        id: 1,
-        username: "pinks",
-        email: "pinks@gmail.com"
-    }
-
-    jwt.sign({ user }, secretKey, { expiresIn: '300s' }, (err, token) => {
-        res.json({
-            token
-        })
-    })
-})
-
-app.post("/profile",verifyToken,(req,res)=>{
-jwt.verify(req.token,secretKey,(err,authData)=>{
-    if(err){
-        res.send({result: "invalid token"})
-    }
-    else{
-        res.json({
-            message: "profile accessed",
-            authData
-        })
-    }
-})
-})
-
-function verifyToken(req, res, next){
-const bearerHeader = req.headers['authorization'];
-
-if(typeof bearerHeader !== 'undefined'){
-    const bearer = bearerHeader.split(" ");
-    const token = bearer[1];
-    req.token = token;
+const verifyJWT = async(req, res, next)=>{
+    const { token } = req.headers;
+   try{
+    const decoded = jwt.verify(token, SECRET_KEY)
     next();
-}
-else{
-    res.send({
-        result: "Token Is Not Valid"
+   }
+   catch(err){
+    return res.json({
+        success: false,
+        message: e.message
     })
+   }
 }
-}
+
+app.post("/login", async(req,res) => {
+
+    const { email } = req.body;
+
+    const token = jwt.sign({email: email, isAdmin: true}, SECRET_KEY, {expiresIn: '1h'})
+
+    res.json({
+        success: true,
+        data: {
+            email: email,
+            jwt_token: token
+        },
+        message: 'logged in successfully!!'
+    })
+})
+
+app.post('/deleteLecture', verifyJWT, (req, res)=>{
+    res.json({
+        success: true,
+        message: 'Lecture deleted successfully!!'
+    })
+})
 
 app.listen(5000, () => {
     console.log("app is sunning on 5000 port");
